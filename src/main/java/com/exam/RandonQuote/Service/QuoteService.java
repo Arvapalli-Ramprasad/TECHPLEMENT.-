2dac492e9508f13e753272214e22245ad60072f4
Ramprasad <ramprasad6352@gmail.com>
2025-01-12 15:55:59 +0530

    added file

--- src/main/java/com/exam/RandonQuote/Service/QuoteService.java
+++ src/main/java/com/exam/RandonQuote/Service/QuoteService.java
@@ -4,9 +4,7 @@ import com.exam.RandonQuote.Models.QuoteModel;
 import com.exam.RandonQuote.Repositories.QuoteRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.Optional;
 import java.util.Random;
 
 @Service
